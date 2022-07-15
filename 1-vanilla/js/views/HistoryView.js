3b4363a054c646f6b92352037f928f5b2beca0f6
yaeji <yaeji0909@gmail.com>
2022-07-15 14:26:06 +0900

    modified

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -1,33 +1,35 @@
-import KeywordView from './KeywordView.js'
+import KeywordView from "./KeywordView.js";
 
-const tag = '[HistoryView]'
+const tag = "[HistoryView]";
 
-const HistoryView = Object.create(KeywordView)
+const HistoryView = Object.create(KeywordView);
 
-HistoryView.messages.NO_KEYWORDS = '검색 이력이 없습니다'
+HistoryView.messages.NO_KEYWORDS = "검색 이력이 없습니다";
 
 HistoryView.getKeywordsHtml = function (data) {
-  return data.reduce((html, item) => {
-    html += `<li data-keyword="${item.keyword}">
+  return (
+    data.reduce((html, item) => {
+      html += `<li data-keyword="${item.keyword}">
       ${item.keyword} 
       <span class="date">${item.date}</span>
       <button class="btn-remove"></button>
-      </li>`
-    return html
-  }, '<ul class="list">') + "</ul>"
-}
+      </li>`;
+      return html;
+    }, '<ul class="list">') + "</ul>"
+  );
+};
 
 HistoryView.bindRemoveBtn = function () {
-  Array.from(this.el.querySelectorAll('button.btn-remove')).forEach(btn => {
-    btn.addEventListener('click', e => {
-      e.stopPropagation()
-      this.onRemove(btn.parentElement.dataset.keyword)
-    })
-  })
-}
+  Array.from(this.el.querySelectorAll("button.btn-remove")).forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      this.onRemove(btn.parentElement.dataset.keyword);
+    });
+  });
+};
 
 HistoryView.onRemove = function (keyword) {
-  this.emit('@remove', { keyword })
-}
+  this.emit("@remove", { keyword });
+};
 
-export default HistoryView
+export default HistoryView;
